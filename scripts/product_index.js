7e485eafb194fd6c4a2e90f7eb6ef1cb399bc756
rudipsq <132471589+rudipsq@users.noreply.github.com>
2024-12-12 20:33:02 +0100

    update

--- scripts/product_index.js
+++ scripts/product_index.js
@@ -1,3 +1,4 @@
+//* enable link to product details page
 document.addEventListener("DOMContentLoaded", () => {
   const rows = document.querySelectorAll("tbody tr");
 
@@ -10,3 +11,32 @@ document.addEventListener("DOMContentLoaded", () => {
     });
   });
 });
+
+//* enaable search bar
+
+const rows = document.querySelectorAll("#product_list tr");
+let searchInput = document.getElementById("search_input");
+
+searchInput.addEventListener("input", function () {
+  let searchString = this.value;
+  filterRows(searchString);
+});
+
+function filterRows(searchString) {
+  const lowerSearchString = searchString.toLowerCase();
+
+  // Loop through each row
+  rows.forEach((row) => {
+    // Get the text content of the row and convert to lowercase
+    const rowText = row.textContent.toLowerCase();
+
+    // Check if the row contains the search string
+    if (rowText.includes(lowerSearchString)) {
+      // If it does, make sure it's visible
+      row.style.display = "";
+    } else {
+      // If it doesn't, hide the row
+      row.style.display = "none";
+    }
+  });
+}
