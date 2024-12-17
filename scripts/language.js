18328567bcea03ce6d65d5019bddd1c2155e3ea9
rudipsq <132471589+rudipsq@users.noreply.github.com>
2024-12-17 21:25:23 +0100

    language test

--- scripts/language.js
+++ scripts/language.js
@@ -1,14 +1,44 @@
-// redirects to a differnet language
-function switchLanguage(languageKey = "en") {
-  const currentLocation = window.location.pathname
-    .split("/")
-    .slice(2)
-    .join("/");
+// // redirects to a differnet language
+// function switchLanguage(languageKey = "en") {
+//   const currentLocation = window.location.pathname
+//     .split("/")
+//     .slice(2)
+//     .join("/");
 
-  window.location.href = "/" + languageKey + "/" + currentLocation;
+//   window.location.href = "/" + languageKey + "/" + currentLocation;
+// }
+
+// // returns the current displayed language key
+// function getLanguage() {
+//   return window.location.href.split("/")[3];
+// }
+
+// Redirects to a different language
+function switchLanguage(languageKey = "en") {
+  const currentPath = window.location.pathname;
+  const pathParts = currentPath.split("/").filter(Boolean);
+  const currentLang = getLanguage();
+
+  if (currentLang) {
+    const langIndex = pathParts.indexOf(currentLang);
+    pathParts[langIndex] = languageKey;
+  } else {
+    pathParts.unshift(languageKey);
+  }
+
+  window.location.href = "/" + pathParts.join("/");
 }
 
-// returns the current displayed language key
+// Returns the current displayed language key
 function getLanguage() {
-  return window.location.href.split("/")[3];
+  const pathParts = window.location.pathname.split("/").filter(Boolean);
+  const languageCodes = ["en", "de"];
+
+  for (const part of pathParts) {
+    if (languageCodes.includes(part)) {
+      return part;
+    }
+  }
+
+  return null;
 }
